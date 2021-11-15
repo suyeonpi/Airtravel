61eaed82b2f7d92e92f0dd34787c0d89273d02dc
suyeon <pisuyeun@gmail.com>
2021-11-15 16:03:53 +0900

    백엔드 기본세팅, DB연결

---
+++ back/config.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+function required(key, defaultValue = undefined) {
+  const value = process.env[key] || defaultValue;
+  if (value == null) {
+    throw new Error(`Key ${key} is undefined`);
+  }
+  return value;
+}
+
+export const config = {
+  jwt: {
+    secretKey: required('JWT_SECRET'),
+    expiresInSec: parseInt(required('JWT_EXPIRES_SEC')),
+  },
+  bcrypt: {
+    saltRounds: parseInt(required('BCRTPY_SALT_ROUNDS')),
+  },
+  host: {
+    port: parseInt(required('HOST_PORT')),
+  },
+  db: {
+    host: required('DB_HOST'),
+  },
+};
