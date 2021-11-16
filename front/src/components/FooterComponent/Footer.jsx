ed7cefb4835a095bde7fa9a96261e5ead7ac509e
Kim doeun <57129686+kimdovi-dev@users.noreply.github.com>
2021-11-17 01:43:37 +0900

    chore (#30): footer 추가

---
+++ front/src/components/FooterComponent/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <div className="footer-wrap">
+          <p>Copyright 2021. Team MetaLibre All rights reserved.</p>
+          <p>Pi Suyeon🌟, Jeon Sanghyuk🧛‍♂️ , Kim Doeun🌜</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
