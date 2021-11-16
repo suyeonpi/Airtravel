c0f9f94603c02ceb2e79a89066883803c83e304b
Kim doeun <57129686+kimdovi-dev@users.noreply.github.com>
2021-11-16 21:27:16 +0900

    chore (#30): 마이페이지 추가 더미데이터 추가

--- front/src/pages/MyPage.jsx
+++ front/src/pages/MyPage.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function MyPage(props) {
-  return <div>마이페이지</div>;
-}
+import PostList from "../components/ListComponent/PostList";
+import userDummy from "../assets/js/userDummy";
+import DummyPost from "../assets/js/DummyPost";
+
+const MyPage = () => {
+  const [userInfo, setUserInfo] = useState([...userDummy]);
+  const [posts, setPosts] = useState([...DummyPost]);
+
+  return (
+    <>
+      <div className="profile">
+        <div className="profile__bg"></div>
+        <div className="profile__info">
+          <img src="" alt="" />
+          <span>{userInfo[0].usernick}</span>
+          <button type="button">프로필 편집</button>
+        </div>
+      </div>
+      <div className="main-content-wrap">
+        <PostList posts={posts} />
+      </div>
+    </>
+  );
+};
 
 export default MyPage;
