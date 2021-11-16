23b3a45692f1d51e703fd3b4e25bcad448f768cb
Kim doeun <57129686+kimdovi-dev@users.noreply.github.com>
2021-11-16 16:21:14 +0900

    feature (#17): 메인 카테고리별 포스트 표시 기능 추가

--- front/src/components/HeaderComponent/Header.jsx
+++ front/src/components/HeaderComponent/Header.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import userDummy from "../../assets/js/userDummy";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPaperPlane,
+  faUserCircle,
+} from "@fortawesome/free-regular-svg-icons";
+import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const [userInfo, setuserInfo] = useState(userDummy);
   return (
     <>
       <header className="header">
@@ -8,8 +16,23 @@ const Header = () => {
           <h1 className="header__logo">
             <a href="/">logo</a>
           </h1>
-          <span className="header__mypage">
-            <a href="#mypage">mypage</a>
+          <span className="header__utils">
+            {userInfo.length === 0 ? (
+              <>
+                <a href="/signup" className="header__signup" title="회원가입">
+                  <FontAwesomeIcon icon={faSignInAlt} />
+                </a>
+                <a href="/login" className="header__login" title="로그인">
+                  <FontAwesomeIcon icon={faUserCircle} />
+                </a>
+              </>
+            ) : (
+              <>
+                <a href="/mypage" className="header__login" title="마이페">
+                  <FontAwesomeIcon icon={faPaperPlane} />
+                </a>
+              </>
+            )}
           </span>
         </nav>
       </header>
