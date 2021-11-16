a770355b4780cf6bb7be357fb0de88134154abb9
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2021-11-16 10:11:47 +0900

    로그인 컴포넌트 로그인 로직 생성 & 로그인 정책 결정 이후에 유효성 검증로직 추가 필요

--- front/src/pages/LoginPage.jsx
+++ front/src/pages/LoginPage.jsx
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LoginPage(props) {
+  const [userID, setuserID] = useState("");
+  const [userPW, setuserPW] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(userID, userPW);
+
+    if (validateLoginForm()) {
+      //[API] [GET] 로그인 요청 보내는 작업.
+      setuserID("");
+      setuserPW("");
+    } else {
+      console.log("input error!");
+      // 클라인어트 사이드 Validation 에러 표시.
+    }
+  };
+
+  const validateLoginForm = () => {
+    //더 강한 클라이언트 사이드 Validation 로직 필요.
+    if (userID.length > 0 && userPW.length > 0) return true;
+  };
+
+  const onChangeUserID = (event) => {
+    setuserID(event.target.value);
+  };
+
+  const onChangeUserPW = (event) => {
+    setuserPW(event.target.value);
+  };
+
   return (
     <div className="login login-container">
-      <div className="login-form">
+      <form onSubmit={onSubmit} className="login-form">
         <div className="login-form title">로그인</div>
         <div className="divider"></div>
-        <input type="text" placeholder="아이디" />
-        <input type="password" placeholder="비밀번호" />
+        <input
+          value={userID}
+          type="text"
+          placeholder="아이디"
+          onChange={onChangeUserID}
+        />
+        <input
+          value={userPW}
+          type="password"
+          placeholder="비밀번호"
+          onChange={onChangeUserPW}
+        />
         <input
           className="btn btn__primary btn__large"
-          type="button"
+          type="submit"
           value="로그인"
         />
         <div className="divider"></div>
@@ -24,7 +64,7 @@ function LoginPage(props) {
             value="회원가입"
           />
         </div>
-      </div>
+      </form>
     </div>
   );
 }
