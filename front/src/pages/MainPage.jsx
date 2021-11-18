ea4ea0a3968637bbfdd1614bf70a452734684112
Kim doeun <57129686+kimdovi-dev@users.noreply.github.com>
2021-11-19 00:01:41 +0900

    feature (#69): cards 정보 호출 API 연동

--- front/src/pages/MainPage.jsx
+++ front/src/pages/MainPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import PostList from "../components/ListComponent/PostList";
 import CategoryList from "../components/ListComponent/CategoryList";
@@ -10,10 +11,21 @@ const MainPage = () => {
   const categories = [...continents];
   const [label, setLabel] = useState(categories[0].continent);
   const onPageTurn = (title) => setLabel((prev) => (prev = title));
-  const [posts, setPosts] = useState([...DummyPost]);
+  const [posts, setPosts] = useState([]);
 
   const onFilterPosts = (label) =>
     DummyPost.filter((item) => item.continent === label);
+  const baseUrl = "http://localhost:3000";
+  // API 호출 용
+  useEffect(() => {
+    const loadPost = async () =>
+      await axios.get(`${baseUrl}/cards`).then((res) => {
+        console.log("data", res.data.data.cards);
+        setPosts([...res.data.data.cards]);
+      });
+    loadPost();
+    return () => {};
+  }, []);
 
   useEffect(() => {
     if (label === "전체") {
