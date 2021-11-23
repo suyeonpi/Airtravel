eb352099ec61f51af0fc64bb7d12030486ce002e
Kim doeun <57129686+kimdovi-dev@users.noreply.github.com>
2021-11-24 00:23:01 +0900

    chore (#111): 게시물 메뉴 모달 작업중

--- front/src/pages/MainPage.jsx
+++ front/src/pages/MainPage.jsx
@@ -4,6 +4,8 @@ import PostList from "../components/ListComponent/PostList";
 import CategoryList from "../components/ListComponent/CategoryList";
 import SliderList from "../components/ListComponent/SliderList";
 
+import PostMenu from "../components/ModalComponent/PostMenu";
+
 import { getCards, continents } from "../apis/cards";
 
 const MainPage = () => {
@@ -45,6 +47,8 @@ const MainPage = () => {
         </div>
         <PostList posts={posts} />
       </div>
+
+      <PostMenu />
     </>
   );
 };
