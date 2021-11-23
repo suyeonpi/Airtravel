6614e7d29e5277ff3dfd5f3023a9d2f3500b27fe
suyeon <pisuyeun@gmail.com>
2021-11-23 22:37:15 +0900

    data validation, sanitization

--- back/routes/commentRouter.js
+++ back/routes/commentRouter.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import * as commentController from '../controllers/commentController.js';
 import { verifyToken } from '../middleware/auth.js';
+import { validateParam } from '../middleware/validator.js';
 
 const router = express.Router();
 
 router
   .route('/:id') // 카드 id
-  .get(verifyToken, commentController.getComments)
-  .post(verifyToken, commentController.createComment);
+  .get(validateParam, verifyToken, commentController.getComments)
+  .post(validateParam, verifyToken, commentController.createComment);
 
 router
   .route('/:id') // 댓글 id
-  .patch(verifyToken, commentController.updateComment)
-  .delete(verifyToken, commentController.deleteComment);
+  .patch(validateParam, verifyToken, commentController.updateComment)
+  .delete(validateParam, verifyToken, commentController.deleteComment);
 
 export default router;
